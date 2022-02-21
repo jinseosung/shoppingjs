40a16f37b95f4165974049c0ab3bd35b93ec1d83
Jinseo Sung <jinseosung15@gmail.com>
2022-02-21 22:54:33 +0100

    Display Items

--- src/main.js
+++ src/main.js
@@ -5,11 +5,26 @@ function loadItems() {
     .then((json) => json.items);
 }
 
+// Update the list with the given items
+function displayItems(items) {
+  const container = document.querySelector(".items");
+  container.innerHTML = items.map((item) => creatHTMLString(item)).join("");
+}
+
+// Create HTML list item from the given item
+function creatHTMLString(item) {
+  return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+        <span class="item__description">${item.gender}, ${item.size}</span>
+      </li>
+      `;
+}
+
 // main
 loadItems()
   .then((items) => {
-    console.log(items);
-    //  displayItems(items);
+    displayItems(items);
     //  setEventListeners(items);
   })
   .catch(console.log);
